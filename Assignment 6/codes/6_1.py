9f4a43934389573fe1033a5f363a381aff8403b9
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-04-25 16:29:29 +0530

    Add files via upload

--- Assignment 6/codes/6_1.py
+++ Assignment 6/codes/6_1.py
@@ -2,29 +2,16 @@
 #April 25, 2022
 #License
 #https://www.gnu.org/licenses/gpl-3.0.en.html
-#To compute the probability regarding the sum
-#of two die given a tablular input of the 
-#possible outcomes.
+#Create a PMF for a single die roll
 
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import stem
 
-#Import Data
-df = pd.read_excel('../tables/intab.xlsx', 'Sheet1')
-A = df.to_numpy()
+#Array for PMF
+X = np.array([1, 2, 3, 4, 5, 6])
+Y = np.array([1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
 
-#Number of possible outcomes
-N = 36
-
-#Number of favourable probabilities
-x_0 = np.count_nonzero(A % 2 == 0)
-x_1 = np.count_nonzero(A == 6)
-x_2 = np.count_nonzero(A >= 6)
-
-#Compute theoretical probabilities
-p_0 = x_0/N
-p_1 = x_1/N
-p_2 = x_2/N
-
-#Display theoretical probabilities
-print("Theoretical probabilities: ", p_0, p_1, p_2)
+#Plotting the PMF
+stem(X, Y, linefmt='k-', markerfmt='ko', basefmt='k-')
+plt.savefig('../figs/6_1.png')
