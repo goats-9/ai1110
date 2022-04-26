2a84422c718008ae3985bea1669b263cd0efd2c1
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-04-26 21:33:04 +0530

    Add files via upload

--- Assignment 6/codes/6_1.py
+++ Assignment 6/codes/6_1.py
@@ -2,7 +2,7 @@
 #April 25, 2022
 #License
 #https://www.gnu.org/licenses/gpl-3.0.en.html
-#Create a PMF for a single die roll
+#Create a PMF and CDF subplot for a single die roll
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +11,22 @@ from matplotlib.pyplot import stem
 #Array for PMF
 X = np.array([1, 2, 3, 4, 5, 6])
 Y = np.array([1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
+Z = np.cumsum(Y)
 
 #Plotting the PMF
+plt.subplot(1, 2, 1)
+plt.xlabel('Value of X')
+plt.ylabel('Probability Mass Function')
+plt.xticks(X)
 stem(X, Y, linefmt='k-', markerfmt='ko', basefmt='k-')
+
+#Plotting the CDF
+plt.subplot(1, 2, 2)
+plt.plot(X, Z, 'ko-')
+plt.xlabel('Value of X')
+plt.ylabel('Cumulative Probability')
+plt.xticks(X)
+plt.grid() # minor
+#plt.axis('equal')
+plt.tight_layout() #Spaace the subplots properly
 plt.savefig('../figs/6_1.png')
