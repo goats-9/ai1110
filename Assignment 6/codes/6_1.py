0ce2b04aeaf8e00667ea437bfcc9385b8238e8b2
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-05-01 05:02:04 +0530

    Upload Assignment 6

--- Assignment 6/codes/6_1.py
+++ Assignment 6/codes/6_1.py
@@ -30,19 +30,30 @@ X = np.array([1, 2, 3, 4, 5, 6])
 Y = np.array([1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
 Z = np.cumsum(Y)
 
+#Extra points
+X_pmf = np.array([0, 7])
+Y_pmf = np.array([0, 0])
+X_cdf = np.array([-1, 0, 7])
+Y_cdf = np.array([0, 0, 1])
+
+#Arrays for ticks
+T = np.array([-1, 0, 1, 2, 3, 4, 5, 6, 7])
+
 #Plotting the PMF
 plt.subplot(1, 2, 1)
 plt.xlabel('Value of X')
 plt.ylabel('Probability Mass Function')
-plt.xticks(X)
-stem(X, Y, linefmt='k-', markerfmt='ko', basefmt='k-')
+plt.xticks(T)
+stem(X, Y, linefmt='k--', markerfmt='ko', basefmt='k-')
+stem(X_pmf, Y_pmf, linefmt='k--', markerfmt='ko', basefmt='k-')
 
 #Plotting the CDF
 plt.subplot(1, 2, 2)
-stem(X, Z, linefmt='k-', markerfmt='ko', basefmt='k-')
+stem(X, Z, linefmt='k--', markerfmt='ko', basefmt='k-')
+stem(X_cdf, Y_cdf, linefmt='k--', markerfmt='ko', basefmt='k-')
 plt.xlabel('Value of X')
-plt.ylabel('Cumulative Probability')
-plt.xticks(X)
+plt.ylabel('Cumulative Distribution Function')
+plt.xticks(T)
 plt.grid()
 plt.tight_layout() #Space the subplots properly
 plt.savefig('../figs/6_1.png')
