ccad49592d3033c69a829b9e62fc829326e605ec
goats-9 <goats.singh@gmail.com>
2022-06-15 09:07:11 +0530

    Add axes, pdf and labels to graph

--- Assignment_14/codes/14_3.py
+++ Assignment_14/codes/14_3.py
@@ -7,10 +7,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def line_gen(A,B):
+    len =10
+    dim = A.shape[0]
+    x_AB = np.zeros((dim,len))
+    lam_1 = np.linspace(0,1,len)
+    for i in range(len):
+        temp1 = A + lam_1[i]*(B-A)
+        x_AB[:,i]= temp1.T
+    return x_AB
+
+
 #Set sample size
 sz = 10000000
 
 mu = np.random.random(size=sz)
 B = np.linspace(0, 1, 100)
 plt.hist(mu, bins=B, density=True)
+A = np.array([0, 1])
+B = np.array([1, 1])
+L_AB = line_gen(A, B)
+plt.axhline(y=0, c='black')
+plt.axvline(x=0, c='black')
+plt.xlabel('x')
+plt.ylabel('$p_X(x)$')
+plt.plot(L_AB[0, :], L_AB[1, :], 'k')
 plt.savefig('../figs/np_random.png')
