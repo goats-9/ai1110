78acb00cc1e4ce8ace5e38c1906c65b99d01258e
goats-9 <goats.singh@gmail.com>
2022-07-05 11:31:21 +0530

    Update plots and pdf

---
+++ simulations/codes/1_1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "1_1.h"
+
+int  main(void) //main function begins
+{
+
+//Sample size
+int sz = 1000000;
+
+//Uniform random numbers
+uniform("../data/uni.dat", sz);
+
+//Gaussian random numbers
+gaussian("../data/gau.dat", 1, sz);
+
+//Triangular random numbers
+triangular("../data/tri.dat", sz);
+//Mean of uniform
+//printf("%lf",mean("uni.dat"));
+
+//5*Bernoulli + Gaussian
+bernoulli("../data/ber.dat", sz);
+double **b = loadtxt("../data/ber.dat", sz, 1);
+double **g = loadtxt("../data/gau.dat", sz, 1);
+FILE *fp = fopen("../data/ber_gau.dat", "w");
+for (int i = 0; i < 1000; i++) { 
+fprintf(fp, "%d\n", 5*b[i][0] + g[i][0]);
+}
+fclose(fp);
+
+//Chi-squared random numbers
+chi2("../data/chi.dat", sz);
+
+//Rayleigh random numbers
+ral("../data/ral.dat", 2, sz);
+
+//Rayleigh*Bernoulli + Gaussian
+//Generate files for Rayleigh Distribution
+//with 0 <= gamma <= 10
+bernoulli("../data/ber_new.dat", sz);
+for (int i = 1; i < 11; i++) { 
+	char file[19] = "../data/ral_xy.dat";
+	file[13] = (i%10) + '0';
+	file[12] = (i/10) + '0';
+	ral(file, i, sz);
+}
+return 0;
+}
---
+++ simulations/codes/5_2.py
@@ -0,0 +1,16 @@
+#Importing numpy, scipy, mpmath and pyplot
+import numpy as np
+import mpmath as mp
+import scipy 
+import matplotlib.pyplot as plt
+
+sz = int(1e6)
+bv = np.loadtxt('../data/ber.dat', dtype='double')
+nv = np.loadtxt('../data/gau.dat', dtype='double')
+sig = 5*bv + nv
+x = np.linspace(0, 1, sz)
+plt.plot(x, sig, '.')
+plt.xlabel('n ($\\times 10^6$)')
+plt.ylabel('y(n)')
+plt.grid()
+plt.savefig('../figs/5_2.png')
